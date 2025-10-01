c23429b20b30cb3d09fc049faa2e10392332b18e
zulpiii <zpskibidi@gmail.com>
2025-10-01 23:44:25 +0800

    Create txc.py

---
+++ ubot/core/plugins/txc.py
@@ -0,0 +1,37 @@
+import asyncio
+import random
+import sleep
+
+from ubot import *
+
+    async def petinggi(client, message):
+    typew = await message.reply("Petinggi Petinggi tai anjing")
+    await asyncio.sleep(2)
+    await typew.edit("Setinggi apasih lu memek?")
+    await asyncio.sleep(1.5)
+    await typew.edit("Cuma Pengangguran aja belagu lo mmk!!!!")
+    await asyncio.sleep(1)
+    await typew.edit("Kerja bego jangan main tele mulu!")
+    await asyncio.sleep(1.5)
+    await typew.edit("MEMEK LU!")
+
+    async def aliansi(client, message):
+    typew = await message.reply("Anak aliansi y lu?")
+    await asyncio.sleep(2)
+    await typew.edit("Mau nanya")
+    await asyncio.sleep(1)
+    await typew.edit("RIBUT KOK DISOSMED ?")
+    await asyncio.sleep(1)
+    await typew.edit("AWOAKWOAKWOAK")
+    await asyncio.sleep(1)
+    await typew.edit("Kabur ah...")
+    await asyncio.sleep(1)
+    await typew.edit("Nanti takut Dirosting")
+    await asyncio.sleep(1)
+    await typew.edit("Terus Nyuruh semua membernya naik ke os")
+    await asyncio.sleep(1.5)
+    await typew.edit("So galak Mic nya ditelen")
+    await asyncio.sleep(1)
+    await typew.edit("Padahal Di Real Life jadi kacung Tongkrongan")
+    await asyncio.sleep(1)
+    await typew.edit("HAHAHA KONTOL!")      
