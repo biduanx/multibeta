546c4ebc6ccef42361b91e26730733ea3456d1f9
zulpiii <zpskibidi@gmail.com>
2025-10-01 19:59:47 +0800

    Update roasting.py

--- ubot/modules/roasting.py
+++ ubot/modules/roasting.py
@@ -8,7 +8,10 @@ __MODULE__ = "Roasting"
 __HELP__ = """
  Bantuan Untuk Roasting
 
-• Perintah: <code>{0}roas</code>
+• Perintah: <code>{0}roast</code>
+• Penjelasan: Untuk caci maki manusia gak tau diri.
+
+• Perintah: <code>{0}norak</code>
 • Penjelasan: Untuk caci maki manusia gak tau diri.
 """
 
@@ -17,4 +20,8 @@ __HELP__ = """
 @PY.UBOT("roast")
 async def _(client, message):
     await roasting_jing(client, message)
+    
+@PY.UBOT("norak")
+async def _(client, message):
+    await roasting_jing(client, message)    
 
