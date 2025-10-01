96aa61c387a7f670f8e4384474b894c6b9262821
zulpiii <zpskibidi@gmail.com>
2025-10-01 20:02:16 +0800

    Update roas.py

--- ubot/core/plugins/roas.py
+++ ubot/core/plugins/roas.py
@@ -12,7 +12,9 @@ roast = [
     "**DAR DER DOR, GC AMPAS KU GEDOR**",
 ]
 
-
+norak = [
+    "**MAEN BOT MULU ALAY, BARU MAKE BOY YA PANTES NORAK!!!**",
+    ]
 
 async def roasting_jing(client, message):
     dia = await extract_user(message)
@@ -22,3 +24,13 @@ async def roasting_jing(client, message):
         return await message.reply("Bot lu mo ilang ?")
     else:
         await message.reply(random.choice(roast))
+        
+async def norak_jing(client, message):
+    dia = await extract_user(message)
+    if dia in DEVS:
+        return await message.reply("Lah si anying mo ilang akun lu ?")
+    elif dia in await get_seles():
+        return await message.reply("Bot lu mo ilang ?")
+    else:
+        await message.reply(random.choice(norak))        
+        
