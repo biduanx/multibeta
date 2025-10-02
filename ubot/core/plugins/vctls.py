203f27ca7da07fc1b6aa35018a8b9925a45f96f2
zulpiii <zpskibidi@gmail.com>
2025-10-03 01:34:04 +0800

    Update vctls.py

--- ubot/core/plugins/vctls.py
+++ ubot/core/plugins/vctls.py
@@ -19,43 +19,58 @@ async def get_group_call(
     chat_peer = await client.resolve_peer(message.chat.id)
     if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
         if isinstance(chat_peer, InputPeerChannel):
-            full_chat = (await client.invoke(GetFullChannel(channel=chat_peer))).full_chat
+            full_chat = (await client.send(GetFullChannel(channel=chat_peer))).full_chat
         elif isinstance(chat_peer, InputPeerChat):
             full_chat = (
-                await client.invoke(GetFullChat(chat_id=chat_peer.chat_id))
+                await client.send(GetFullChat(chat_id=chat_peer.chat_id))
             ).full_chat
         if full_chat is not None:
             return full_chat.call
     await eor(message, f"**No group call Found** {err_msg}")
     return False
 
-
-async def start_vctools(client, message):
-    ky = await message.reply("<code>Processing....</code>")
-    chat_id = message.chat.id
+async def start_vctools(client: Client, message: Message):
+    flags = " ".join(message.command[1:])
+    ky = await eor(message, "`Processing....`")
+    vctitle = get_arg(message)
+    if flags == enums.ChatType.CHANNEL:
+        chat_id = message.chat.title
+    else:
+        chat_id = message.chat.id
+    args = f"<b>Obrolan Suara Aktif</b>\n • <b>Chat</b> : {message.chat.title}"
     try:
-        await client.invoke(
-            CreateGroupCall(
-            peer=(await client.resolve_peer(chat_id)),
-            random_id=randint(0, 2147483647),
+        if not vctitle:
+            await client.invoke(
+                CreateGroupCall(
+                    peer=(await client.resolve_peer(chat_id)),
+                    random_id=randint(10000, 999999999),
+                )
             )
-        )
-        await ky.edit(f"<b>• Obrolan Suara Aktif</b>\n<b>• Chat : </b><code>{message.chat.title}</code>")
+        else:
+            args += f"\n • <b>Title:</b> {vctitle}"
+            await client.invoke(
+                CreateGroupCall(
+                    peer=(await client.resolve_peer(chat_id)),
+                    random_id=randint(10000, 999999999),
+                    title=vctitle,
+                )
+            )
+        await ky.edit(args)
     except Exception as e:
-        await ky.edit(f"<b>Error:</b> <code{e}</code>")
+        await ky.edit(f"<b>INFO:</b> `{e}`")
 
 
 
-async def stop_vctools(client, message):
-    ky = await message.reply("<code>Processing....</code>")
+async def stop_vctools(client: Client, message: Message):
+    ky = await eor(message, "`Processing....`")
     message.chat.id
     if not (
         group_call := (await get_group_call(client, message, err_msg=", Kesalahan..."))
     ):
         return
-    await client.invoke(DiscardGroupCall(call=group_call))
+    await client.send(DiscardGroupCall(call=group_call))
     await ky.edit(
-        f"<b>• Obrolan Suara Diakhiri</b>\n<b>• Chat : </b><code>{message.chat.title}</code>"
+        f"<b>Obrolan Suara Diakhiri</b>\n • <b>Chat</b> : {message.chat.title}"
     )
 
 
