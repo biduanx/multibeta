f69c2c41a6949198902a4637973ad99e2d480d13
zulpiii <zpskibidi@gmail.com>
2025-10-02 16:29:22 +0800

    Update __init__.py

--- ubot/core/plugins/__init__.py
+++ ubot/core/plugins/__init__.py
@@ -56,6 +56,7 @@ from ubot.core.plugins.pntrst import *
 from ubot.core.plugins.rmndr import *
 from ubot.core.plugins.wbs import *
 from ubot.core.plugins.anm import *
+from ubot.core.plugins.anm2 import *
 from ubot.core.plugins.crpt import *
 from ubot.core.plugins.bwt import *
 from ubot.core.plugins.roas import *
