576927da31f310603c82d24a2fd1173f4f8740c4
zulpiii <zpskibidi@gmail.com>
2025-10-04 00:48:24 +0800

    Add files via upload

---
+++ ubot/core/plugins/anm3.py
@@ -0,0 +1,64 @@
+"""
+Amang Kontol
+"""
+import asyncio
+import random
+
+import requests
+from pyrogram import *
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram.types import *
+
+from ubot import *
+
+
+
+async def per(client, message):
+    e = await message.edit("**WOIII TOLOL**")
+    await asyncio.sleep(1.5)
+    await e.edit("**EMAK KAU LACUR TELE**")
+    await asyncio.sleep(1.5)
+    await e.edit("**SOSOAN NGEBACOT**")
+    await asyncio.sleep(1.5)
+    await e.edit("**MERASA PALING JAGO**")
+    await asyncio.sleep(1.5)
+    await e.edit("**KEREN LU BEGITU ?**")
+    await asyncio.sleep(1.5)
+    await e.edit("**LU ITU CUMA PECUNDANG**")
+    await asyncio.sleep(1.5)
+    await e.edit("**YANG GAPUNYA**")
+    await asyncio.sleep(1.5)
+    await e.edit("**HARGA DIRI**")
+    await asyncio.sleep(1.5)
+    await e.edit("**CUIHHHHH**")    
+    await asyncio.sleep(1.5)
+    await e.edit("**HAHAHAHA**")        
+    await asyncio.sleep(1)
+    await e.delete()        
+
+async def kendu(client, message):
+    e = await message.edit("**Ayanggg 😖**")
+    await asyncio.sleep(1.5)
+    await e.edit("**Kendu Yuggg 😋**")
+    await asyncio.sleep(1.5)
+    await e.edit("**Lagi Birahi Nih 🥵**")
+    await asyncio.sleep(1.5)
+    await e.edit("**AHT AHT AHT**")
+    await asyncio.sleep(1.5)
+    await e.edit("**Pengen Di Goyang Sama kamu 🥵**")
+    await asyncio.sleep(1)
+    await e.delete()    
+    
+async def cinta(client, message):
+    e = await message.edit("**menemukan cinta sejati**")
+    await asyncio.sleep(1.5)
+    await e.edit("**bukan lah hal yang gampang**")
+    await asyncio.sleep(1.5)
+    await e.edit("**menemukan cinta sejati**")
+    await asyncio.sleep(1.5)
+    await e.edit("**harus di ranjang**")
+    await asyncio.sleep(1.5)
+    await e.edit("**goyang bersama**")
+    await asyncio.sleep(1)
+    await e.delete()    
+    
