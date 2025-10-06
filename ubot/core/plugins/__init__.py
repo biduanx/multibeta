dd41044b4e13ef3e81c64a71db85415f10aba88d
zulpiii <zpskibidi@gmail.com>
2025-10-07 01:28:11 +0800

    Update __init__.py

--- ubot/core/plugins/__init__.py
+++ ubot/core/plugins/__init__.py
@@ -1,4 +1,4 @@
-#from ubot.core.plugins.ag imvctl
+from ubot.core.plugins.ag import *
 from ubot.core.plugins.join import *
 from ubot.core.plugins.crbn import *
 from ubot.core.plugins.prof import *
