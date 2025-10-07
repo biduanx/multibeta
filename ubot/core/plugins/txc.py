b1667e615e80a0b4a8ab4878fbbd6c647990ab12
zulpiii <zpskibidi@gmail.com>
2025-10-08 01:08:18 +0800

    Update txc.py

--- ubot/core/plugins/txc.py
+++ ubot/core/plugins/txc.py
@@ -4,15 +4,46 @@ Amang Kontol
 import asyncio
 import random
 from random import choice
-from time import sleep
 from ubot import *
 
 
-async def _(event):
-    await event.eor("GA USAH NGATAIN ORANG TOLOL, MUKA LU AJA KEK GEMBEL TEXAS GOBLOK!!")
-    
-async def _(event):
-    await event.eor("KONTOL MASIH ITEM AJA SO SO AN NGAJAK VCS ANJING?")
+pp = [
+    "**PP LU KEMANA DAH? LAGI DEPRESI? APA GIMANA ? AWOAKWOAK 😆**",
+]
 
-async def _(event):
-    await event.eor("MEMEK LU ITEM ANJENGGG BAU TERASI IDIHHHH!!")    
+kn = [
+    "**KONTOL MASIH ITEM AJA SO SO AN NGAJAK VCS ANJING ?**",
+]
+
+mk = [
+    "**SOSOAN OPEN VCS MEMEK LU INTEM ANJENGGG BAU TERASI IDIHHHH!!**",
+]
+
+
+
+async def pp_jing(client, message):
+    dia = await extract_user(message)
+    if dia in DEVS:
+        return await message.reply("Lah si anying mo ilang akun lu ?")
+    elif dia in await get_seles():
+        return await message.reply("Bot lu mo ilang ?")
+    else:
+        await message.reply(random.choice(pp))
+        
+async def kn_jing(client, message):
+    dia = await extract_user(message)
+    if dia in DEVS:
+        return await message.reply("Lah si anying mo ilang akun lu ?")
+    elif dia in await get_seles():
+        return await message.reply("Bot lu mo ilang ?")
+    else:
+        await message.reply(random.choice(kn))
+        
+async def mk_jing(client, message):
+    dia = await extract_user(message)
+    if dia in DEVS:
+        return await message.reply("Lah si anying mo ilang akun lu ?")
+    elif dia in await get_seles():
+        return await message.reply("Bot lu mo ilang ?")
+    else:
+        await message.reply(random.choice(mk))                        
