ac91e4060807653b19af62838175c787fe3845f9
zulpiii <zpskibidi@gmail.com>
2025-10-08 01:09:14 +0800

    Update toxic.py

--- ubot/modules/toxic.py
+++ ubot/modules/toxic.py
@@ -8,12 +8,26 @@ __MODULE__ = "Toxic"
 __HELP__ = """
  Bantuan Untuk Toxic
 
-• Perintah: <code>{0}roas</code>
+• Perintah: <code>{0}pp</code>
+• Penjelasan: Untuk caci maki manusia gak tau diri.
+
+• Perintah: <code>{0}kn</code>
+• Penjelasan: Untuk caci maki manusia gak tau diri.
+
+• Perintah: <code>{0}mk</code>
 • Penjelasan: Untuk caci maki manusia gak tau diri.
 """
 
 
 
-@PY.UBOT("roas")
+@PY.UBOT("pp")
 async def _(client, message):
-    await roasting_jing(client, message)
+    await pp_jing(client, message)
+
+@PY.UBOT("kn")
+async def _(client, message):
+    await kn_jing(client, message)
+
+@PY.UBOT("mk")
+async def _(client, message):
+    await mk_jing(client, message)
