274a44cf975e4beb0a6b6d6429298bd83e95e9b3
zulpiii <zpskibidi@gmail.com>
2025-10-08 03:36:40 +0800

    Update txc.py

--- ubot/core/plugins/txc.py
+++ ubot/core/plugins/txc.py
@@ -1,49 +1,88 @@
-"""
-Amang Kontol
-"""
 import asyncio
 import random
-from random import choice
+
+import requests
+from pyrogram import *
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram.types import *
+
 from ubot import *
 
 
-pp = [
-    "**PP LU KEMANA DAH? LAGI DEPRESI? APA GIMANA ? AWOAKWOAK 😆**",
-]
+async def title(client, message):
+    xx = await message.edit("**OI ANAK TITLE**")    
+    await asyncio.sleep(2)
+    await xx.edit("**OOO INI YANG SOK JADI PAHLAWAN DI TELEKRAM ?**")
+    await asyncio.sleep(2)
+    await xx.edit("**TITLE KEMANA MANA SAMPE MENUHIN NAMA**")
+    await asyncio.sleep(2)
+    await xx.edit("**ADA YANG SAMPE 18+ LAH SEGALA MACEM**")
+    await asyncio.sleep(2)
+    await xx.edit("**LO KIRA KEREN KEK GITU?**")
+    await asyncio.sleep(2)
+    await xx.edit("**KERJAAN CUMA NGURUSIN GRUP DI TELEGRAM SAMA NGAJAK ORANG WAR**")
+    await asyncio.sleep(2)
+    await xx.edit("**YAELAH BRO MENTAL LO CUMA DI SOSMED APA GIMANE ?**")
+    await asyncio.sleep(2)    
+    await xx.edit("**GILIRAN TITLE NYA DI EJEK NGADU KE OWNER NYA**")
+    await asyncio.sleep(2)
+    await xx.edit("**TRUS NGAJAK WAR**")
+    await asyncio.sleep(2)
+    await xx.edit("**BUSET DAH BANG**")
+    await asyncio.sleep(2)
+    await xx.edit("**UDAH SEJAGO APESI SAMPE GC DIBELA BELA**")
+    await asyncio.sleep(2)
+    await xx.edit("**ORANG TUA LO NOH ADA YANG NAGIH UTANG UDA LO BELA BELOM ?**")
+    await asyncio.sleep(2)
+    await xx.edit("**RELA NGUTANG DEMI NGIDUPIN LU**")
+    await asyncio.sleep(2)
+    await xx.edit("**EH ANAKNYA MALAH NGEBELAIN GC GAJELAS HAHAHA**")
+    await asyncio.sleep(2)
+    await xx.edit("**MANA VIRTUAL LAGI, SOK JAGO LAGI DUH**")
+    await asyncio.sleep(2)
+    await xx.edit("**SEMOGA CEPET SADAR YA HAHAHAHA**")
+    await asyncio.sleep(2)
+    await xx.delete()
 
-kn = [
-    "**KONTOL MASIH ITEM AJA SO SO AN NGAJAK VCS ANJING ?**",
-]
+async def wah(client, message):
+    xx = await message.edit("**Wahh, Jagoan keren bang**")       
+    await asyncio.sleep(2)
+    await xx.edit("**Tapi, Kok Kaya lawakan**")
+    await asyncio.sleep(2)
+    await xx.edit("**Oh iya, Kan lo badut 🤡**")
+    await asyncio.sleep(2)
+    await xx.edit("`Kesannya lo ngasih kita hiburan.`")
+    await asyncio.sleep(2)
+    await xx.edit("`Kasian badut🤡, Ga di hargain, Eh lampiaskan nya ke Tele, Wkwkwk`")
+    await asyncio.sleep(2)
+    await xx.edit("`Dah sana cabut, Makasih hiburannya, Udah bikin Gua tawa ngakak`")
+    await asyncio.sleep(2)
+    await xx.delete()    
+    
+async def ewe(client, message):
+    xx = await message.edit("**Ayanggggg😖**")           
+    await asyncio.sleep(1)
+    await xx.edit("`Ange😫`")
+    await asyncio.sleep(1)
+    await xx.edit("`Ayukkk Ewean Ayanggg🤤`")    
+    await asyncio.sleep(2)
+    await xx.delete()   
 
-mk = [
-    "**SOSOAN OPEN VCS MEMEK LU INTEM ANJENGGG BAU TERASI IDIHHHH!!**",
-]
-
-
-
-async def pp_jing(client, message):
-    dia = await extract_user(message)
-    if dia in DEVS:
-        return await message.reply("Lah si anying mo ilang akun lu ?")
-    elif dia in await get_seles():
-        return await message.reply("Bot lu mo ilang ?")
-    else:
-        await message.reply(random.choice(pp))
-        
-async def kn_jing(client, message):
-    dia = await extract_user(message)
-    if dia in DEVS:
-        return await message.reply("Lah si anying mo ilang akun lu ?")
-    elif dia in await get_seles():
-        return await message.reply("Bot lu mo ilang ?")
-    else:
-        await message.reply(random.choice(kn))
-        
-async def mk_jing(client, message):
-    dia = await extract_user(message)
-    if dia in DEVS:
-        return await message.reply("Lah si anying mo ilang akun lu ?")
-    elif dia in await get_seles():
-        return await message.reply("Bot lu mo ilang ?")
-    else:
-        await message.reply(random.choice(mk))                        
+async def texas(client, message):
+    xx = await message.edit("**Gausa Ngatain Orang**")       
+    await asyncio.sleep(2)
+    await xx.edit("**Muka Lo Aja**")
+    await asyncio.sleep(2)
+    await xx.edit("**Kayak Gembel TEXAS GBLOK**")
+    await asyncio.sleep(2)
+    await xx.delete()    
+    
+async def item(client, message):
+    xx = await message.edit("**KONTOL MASIH ITEM AJA SOSOAN NGAJAK VCS ANJING ?**")       
+    await asyncio.sleep(3)  
+    await xx.delete()                 
+    
+async def memeg(client, message):
+    xx = await message.edit("**SOSOAN OPEN VCS MEMEK LU INTEM ANJENGGG BAU TERASI IDIHHHH!!**")       
+    await asyncio.sleep(3)  
+    await xx.delete()                
