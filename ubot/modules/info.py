e64e3b7966ab4870771fc09e35f43f381e76a1ca
zulpiii <zpskibidi@gmail.com>
2025-10-10 04:11:06 +0800

    Update info.py

--- ubot/modules/info.py
+++ ubot/modules/info.py
@@ -2,13 +2,14 @@ from ubot import *
 
 __MODULE__ = "Info"
 __HELP__ = """
- Bantuan Untuk Info
+<blockquote> Bantuan Untuk Info
 
 • Perintah : <code>{0}info</code> [user_id/username/balas pesan]
 • Penjelasan : Untuk melihat informasi pengguna.
 
 • Perintah : <code>{0}cinfo</code> [user_id/username/balas pesan]
 • Penjelasan : Untuk melihat informasi obrolan.
+</blockquote>
 """
 
 
