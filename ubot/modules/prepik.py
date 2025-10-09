5696bb44d277d0c474690810d8a9d4af16033e14
zulpiii <zpskibidi@gmail.com>
2025-10-10 04:06:40 +0800

    Update prepik.py

--- ubot/modules/prepik.py
+++ ubot/modules/prepik.py
@@ -2,13 +2,15 @@ from ubot import *
 
 __MODULE__ = "Prefix"
 __HELP__ = """
- Bantuan Untuk Prefix
+<blockquote> Bantuan Untuk Prefix
 
 • Perintah : {0}prefix [trigger]
 • Penjelasan : Untuk mengatur handler userbot anda.
+</blockquote>
 """
 
 
 @PY.UBOT("prefix")
 async def _(client, message):
+
     await kok_anjeng(client, message)
