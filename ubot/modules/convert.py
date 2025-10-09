20d9ec3256fdc31ab7826b03095891e0bafa03dc
zulpiii <zpskibidi@gmail.com>
2025-10-10 03:48:57 +0800

    Update convert.py

--- ubot/modules/convert.py
+++ ubot/modules/convert.py
@@ -2,7 +2,7 @@ from ubot import *
 
 __MODULE__ = "Convert"
 __HELP__ = """
- Bantuan Untuk Convert
+<blockquote> Bantuan Untuk Convert
  
 • Perintah : <code>{0}toimg</code> [balas stiker/gif]
 • Penjelasan : Merubah stiker/gif ke foto.
@@ -18,6 +18,7 @@ __HELP__ = """
 
 • Perintah : <code>{0}curi</code> [balas pesan]
 • Penjelasan : Untuk mencuri media timer, cek pesan tersimpan
+</blockquote>
 """
 
 
