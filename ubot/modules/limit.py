10991608c6d035c41dc6b8aab6eb8edb2619451e
zulpiii <zpskibidi@gmail.com>
2025-10-10 04:09:40 +0800

    Update limit.py

--- ubot/modules/limit.py
+++ ubot/modules/limit.py
@@ -2,10 +2,11 @@ from ubot import *
 
 __MODULE__ = "Limit"
 __HELP__ = """
- Bantuan Untuk Limit
+<blockquote> Bantuan Untuk Limit
 
 • Perintah : <code>{0}limit</code>
 • Penjelasan : Untuk mengecek akun anda terbatas atau tidak.
+</blockquote>
 """
 
 
