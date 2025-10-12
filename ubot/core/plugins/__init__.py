a5b4225c6ed70c341d38a505a022280cd89a95c8
zulpiii <zpskibidi@gmail.com>
2025-10-13 04:58:51 +0800

    Update __init__.py

--- ubot/core/plugins/__init__.py
+++ ubot/core/plugins/__init__.py
@@ -1,4 +1,4 @@
-from ubot.core.plugins.join import *
+#from ubot.core.plugins.join import *
 from ubot.core.plugins.crbn import *
 from ubot.core.plugins.prof import *
 from ubot.core.plugins.ip import *
