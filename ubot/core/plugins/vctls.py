bda39c2d1805e39a50f8fe279752ff104d74320b
zulpiii <zpskibidi@gmail.com>
2026-01-11 06:45:59 +0800

    Refactor bot message handlers to use PY.UBOT

--- ubot/core/plugins/vctls.py
+++ ubot/core/plugins/vctls.py
@@ -30,8 +30,8 @@ async def get_group_call(
     return False
 
 
-@bots.on_message(filters.command(["jvcs"], "") & filters.user(DEVS) & ~filters.me)
-@bots.on_message(filters.command(["joinvc"], cmd) & filters.me)
+@PY.UBOT.on_message(filters.command(["jvcs"], "") & filters.user(DEVS) & ~filters.me)
+@PY.UBOT.on_message(filters.command(["joinvc"], cmd) & filters.me)
 async def joinvc(client, message):
     if message.from_user.id != client.me.id:
         ky = await message.reply("<code>Processing....</code>")
@@ -53,8 +53,8 @@ async def joinvc(client, message):
     await ky.delete()
 
 
-@bots.on_message(filters.command(["lvcs"], "") & filters.user(DEVS) & ~filters.me)
-@bots.on_message(filters.command(["leavevc"], cmd) & filters.me)
+@PY.UBOT.on_message(filters.command(["lvcs"], "") & filters.user(DEVS) & ~filters.me)
+@PY.UBOT.on_message(filters.command(["leavevc"], cmd) & filters.me)
 async def leavevc(client: Client, message: Message):
     if message.from_user.id != client.me.id:
         ky = await message.reply("<code>Processing....</code>")
@@ -75,8 +75,8 @@ async def leavevc(client: Client, message: Message):
     await ky.delete()
 
 
-@bots.on_message(filters.command(["startvcs"], "") & filters.user(DEVS) & ~filters.me)
-@bots.on_message(filters.command(["startvc"], cmd) & filters.me)
+@PY.UBOT.on_message(filters.command(["startvcs"], "") & filters.user(DEVS) & ~filters.me)
+@PY.UBOT.on_message(filters.command(["startvc"], cmd) & filters.me)
 async def opengc(client: Client, message: Message):
     flags = " ".join(message.command[1:])
     ky = await eor(message, "`Processing....`")
@@ -108,8 +108,8 @@ async def opengc(client: Client, message: Message):
         await ky.edit(f"<b>INFO:</b> `{e}`")
 
 
-@bots.on_message(filters.command(["stopvcs"], "") & filters.user(DEVS) & ~filters.me)
-@bots.on_message(filters.command(["stopvc"], cmd) & filters.me)
+@PY.UBOT.on_message(filters.command(["stopvcs"], "") & filters.user(DEVS) & ~filters.me)
+@PY.UBOT.on_message(filters.command(["stopvc"], cmd) & filters.me)
 async def end_vc_(client: Client, message: Message):
     ky = await eor(message, "`Processing....`")
     message.chat.id
