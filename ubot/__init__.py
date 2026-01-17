ba6d23ccbd0cde54de9ea65193cab0a06cf8b501
zulpiii <zpskibidi@gmail.com>
2026-01-17 13:30:28 +0800

    Update __init__.py

--- ubot/__init__.py
+++ ubot/__init__.py
@@ -184,9 +184,7 @@ bot = Bot(
     bot_token=BOT_TOKEN,
 )
 
-for bot in bots:
-    if not hasattr(bot, "group_call"):
-        setattr(bot, "group_call", GroupCallFactory(bot).get_group_call())
+
         
 from ubot.core.database import *
 from ubot.core.function import *
