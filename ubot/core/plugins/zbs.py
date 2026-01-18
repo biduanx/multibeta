175fdc90ddcd2f0de1a2bdea77705d195624e430
zulpiii <zpskibidi@gmail.com>
2026-01-19 03:59:33 +0800

    Update zbs.py

--- ubot/core/plugins/zbs.py
+++ ubot/core/plugins/zbs.py
@@ -1,27 +1,18 @@
-async def _(client, message):
+async def zombies_cmd(client, message):
     chat_id = message.chat.id
     deleted_users = []
     banned_users = 0
-    m = await eor(
-        message,
-        f"{emo.proses} <b>Sedang mencari akun-akun depresi ditinggal kawin...</b>",
-    )
-    await sleep(2)
-
+    Tm = await message.reply("<code>sᴇᴅᴀɴɢ ᴍᴇᴍᴇʀɪᴋsᴀ</code>")
     async for i in client.get_chat_members(chat_id):
         if i.user.is_deleted:
             deleted_users.append(i.user.id)
     if len(deleted_users) > 0:
         for deleted_user in deleted_users:
             try:
+                banned_users += 1
                 await message.chat.ban_member(deleted_user)
             except Exception:
                 pass
-            banned_users += 1
-        await m.edit(
-            f"{emo.sukses} <b>Berhasil mengkawinkan {banned_users} Akun Depresi Ditinggal Kawin.</b>"
-        )
+        await Tm.edit(f"<b>ʙᴇʀʜᴀsɪʟ ᴍᴇɴɢᴇʟᴜᴀʀᴋᴀɴ {banned_users} ᴀᴋᴜɴ ᴛᴇʀʜᴀᴘᴜs</b>")
     else:
-        await m.edit(
-            f"{emo.gagal} <b>Saya tidak menemukan akun depresi di tinggal kawin.</b>"
-        )
+        await Tm.edit("<b>ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴀᴋᴜɴ ᴛᴇʀʜᴀᴘᴜs ᴅɪ ɢʀᴏᴜᴘ ɪɴɪ</b>")
