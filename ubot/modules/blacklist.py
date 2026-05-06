2eb2ff4960c7ca5cbeb55f76b5475208e31cbe0e
zulpiii <zpskibidi@gmail.com>
2026-05-07 03:15:15 +0800

    Update blacklist.py

--- ubot/modules/blacklist.py
+++ ubot/modules/blacklist.py
@@ -1,8 +1,8 @@
 from ubot import *
 
 __MODULE__ = "Addbl"
-__HELP__ = """
-<blockquote> Bantuan Untuk Blacklist
+__HELP__ = <blockquote>"""
+ Bantuan Untuk Blacklist
 
 • Perintah : <code>{0}rallbl</code>
 • Penjelasan : Menghapus semua anti gcast
@@ -15,8 +15,7 @@ __HELP__ = """
 
 • Perintah : <code>{0}listbl</code>
 • Penjelasan : Melihat daftar grup anti Gcast.
-</blockquote>
-"""
+"""</blockquote>
 
 
 @PY.UBOT("addbl", FILTERS.ME_GROUP)
