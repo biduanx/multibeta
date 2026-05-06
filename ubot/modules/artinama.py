c3440b558248629b4d08cd66b69dc6cb9d0e2020
zulpiii <zpskibidi@gmail.com>
2026-05-07 04:45:16 +0800

    Update artinama.py

--- ubot/modules/artinama.py
+++ ubot/modules/artinama.py
@@ -1,12 +1,12 @@
 from ubot import *
 
 __MODULE__ = "Artinama"
-__HELP__ = <blockquote>"""
-Bantuan Untuk Arti Nama
+__HELP__ = """
+<blockquote><b>Bantuan Untuk Arti Nama</b>
 
 Perintah:
-<code>{0}artinama nama</code> → Mengartikan dengan nama
-"""</blockquote></b>
+<code>{0}artinama [nama]</code> → Mengartikan dengan nama</blockquote></b>
+"""
 
 
 @PY.UBOT("artinama")
