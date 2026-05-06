58bb4baefbe5979fd5ad8e42782208c2f7bbb741
zulpiii <zpskibidi@gmail.com>
2026-05-07 03:37:16 +0800

    Update toxic3.py

--- ubot/modules/toxic3.py
+++ ubot/modules/toxic3.py
@@ -5,8 +5,8 @@ Amwang Kontol
 from ubot import *
 
 __MODULE__ = "Toxic3"
-__HELP__ = """
-<blockquote> Bantuan Untuk Toxic3
+__HELP__ = <blockquote>"""
+Bantuan Untuk Toxic3
 
 • Perintah: <code>{0}aliansi</code>
 • Penjelasan: coba sendiri mek.
@@ -28,8 +28,7 @@ __HELP__ = """
 
 • Perintah: <code>{0}war</code>
 • Penjelasan: coba sendiri mek.
-</blockquote>
-"""
+"""</blockquote>
 
 
 
